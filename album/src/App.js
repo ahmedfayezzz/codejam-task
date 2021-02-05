d0e1a22751a96538829f124255ce8a548ede17c5
ahmedfayezzz <ahmedfayezmine@gmail.com>
2021-02-05 23:09:32 +0200

    added navbar

--- album/src/App.js
+++ album/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/global.css";
+import NavBar from "./components/navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import AlbumView from "./pages/albumView";
+import AlbumList from "./pages/albumList";
 
 function App() {
   return (
     <>
-      
+      <header>
+        <NavBar />
+      </header>
+      <Router>
+        <Switch>
+          <Route path="/albumlist" component={AlbumList} />
+          <Route path="/albums/:id" component={AlbumView} />
+          <Redirect from="/" to="/albumlist" />
+        </Switch>
+      </Router>
     </>
   );
 }
---
+++ album/src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/navbar.module.css'
+
+const NavBar = () => {
+  return ( 
+  <nav className={styles.container}>
+    <h1 className={styles.logo}>Photo Album App</h1>
+  </nav> );
+}
+ 
+export default NavBar
